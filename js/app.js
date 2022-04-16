eb82a1d4241437003566fc64878c0bcb6607c39a
alcb1310 <97707578+alcb1310@users.noreply.github.com>
2022-04-16 16:40:27 -0500

    finalized project

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const selectedNumberEl = document.getElementById("selected-number");
+
+document.getElementById("js-btn-1").addEventListener("click", () => {
+  selectedNumberEl.textContent = 1;
+  document.getElementById("ratings").style.display = "none";
+  document.getElementById("results").style.display = "flex";
+});
+
+document.getElementById("js-btn-2").addEventListener("click", () => {
+  selectedNumberEl.textContent = 2;
+  document.getElementById("ratings").style.display = "none";
+  document.getElementById("results").style.display = "flex";
+});
+
+document.getElementById("js-btn-3").addEventListener("click", () => {
+  selectedNumberEl.textContent = 3;
+  document.getElementById("ratings").style.display = "none";
+  document.getElementById("results").style.display = "flex";
+});
+
+document.getElementById("js-btn-4").addEventListener("click", () => {
+  selectedNumberEl.textContent = 4;
+  document.getElementById("ratings").style.display = "none";
+  document.getElementById("results").style.display = "flex";
+});
+
+document.getElementById("js-btn-5").addEventListener("click", () => {
+  selectedNumberEl.textContent = 5;
+  document.getElementById("ratings").style.display = "none";
+  document.getElementById("results").style.display = "flex";
+});
